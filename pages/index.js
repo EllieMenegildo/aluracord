1d455501ac1cc5c4741a032241d1c37e711f33e7
EllieMenegildo <dmenegildo091@gmail.com>
2022-01-28 18:51:50 -0300

    Update aluracord

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import {useRouter} from 'next/router';
 import appConfig from '../config.json';
 
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-           *{
-               margin: 0;
-               padding:0;
-               box-sizing: border-box;
-               list-style: none;
-           }
-
-           body{
-               font-family: 'Open Sans', sans-serif;
-           }
-
-           /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-
-        `}</style>
-    );
-}
 
 function Titulo(props) {
     console.log(props.children);
@@ -67,10 +37,13 @@ function Titulo(props) {
 //export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    //const username = 'peas';
+    const [username, setUsername]= React.useState('peas');
+    const roteamento = useRouter();
+
     return (
         <>
-            <GlobalStyle />
+            
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -96,6 +69,12 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function(infosDoEvento){
+                            infosDoEvento.preventDefault();
+                            console.log("alguem submeteu o form!!")
+                           // window.location.href = '/chat';
+                            roteamento.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -106,7 +85,28 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
 
+                      {/*  <input type="text" 
+                            value={username}
+                            onChange={function(event){
+                                console.log("usuário escreveu!", event.target.value);
+                                //Onde está o valor?
+                                const valor = event.target.value
+                                //Trocar o valor da variável (através do React, avisando quem precisa)
+                                setUsername(valor);
+                            }
+                        }
+                            />*/}
+
                         <TextField
+                        value={username}
+                        onChange={function(event){
+                            console.log("usuário escreveu!", event.target.value);
+                            //Onde está o valor?
+                            const valor = event.target.value
+                            //Trocar o valor da variável (através do React, avisando quem precisa)
+                            setUsername(valor);
+                        }
+                    }
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -116,7 +116,7 @@ export default function PaginaInicial() {
                                     backgroundColor: appConfig.theme.colors.neutrals[800],
                                 },
                             }}
-                        />
+                        /> 
                         <Button
                             type='submit'
                             label='Entrar'
